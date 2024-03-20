ddf7a405b8446491b3e41f5006dca2c641c405b8
Hamza <hamzaciao90@gmail.com>
2024-03-20 12:20:37 +0100

    fix register

--- src/service/ConnectionService.jsx
+++ src/service/ConnectionService.jsx
@@ -1,28 +1,34 @@
 import axios from "axios";
 const userString = "User";
 export const RegisterService = async(payload)  => {
-    try {
-        const response = await axios.post(
-            `${process.env.REACT_APP_URL}${userString}/login`,
-            {
-                email: payload.email,
-                password: payload.password,
-            },
-            {
-                headers: {
-                    'Content-Type': 'application/json',
+        try {
+            const response = await axios.post(
+                `${process.env.REACT_APP_URL}${userString}/register`,
+                {
+                    lastName: payload.lastName,
+                    firstName: payload.firstName,
+                    email: payload.email,
+                    password: payload.password,
                 },
-            }
-        );
-        const expirationDate = new Date();
-        expirationDate.setTime(expirationDate.getTime() + (2 * 60 * 60 * 1000));
+                {
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                }
+            );
 
-        document.cookie = `token=${response.data.result}; Path=/; Expires=${expirationDate.toUTCString()};`
-        return response;
-    } catch (error) {
-        return error;
-    }
-}
+            const expirationDate = new Date();
+            expirationDate.setTime(expirationDate.getTime() + (2 * 60 * 60 * 1000));
+
+            document.cookie = `token=${response.data.result}; Path=/; Expires=${expirationDate.toUTCString()};`
+
+            return response;
+        } catch (error) {
+            return error;
+        }
+    };
+
+
 
 export const LoginService = async(payload)  => {
     try {
